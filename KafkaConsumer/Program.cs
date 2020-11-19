1e158c8397dced0e29ebb51fa819c12fec515e7a
Ameer Thasthahir <ameer.thasthahir@aspiresys.com>
2020-11-19 12:22:08 +0530

    Add project files.

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace KafkaConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kafkaEndpoint = "localhost:9092";
+            string kafkaTopic = "javainuse-topic";
+            // Create the consumer configuration
+            var consumerConfig = new Dictionary<string, object>
+            {
+                { "group.id", "myconsumer" },
+                { "bootstrap.servers", kafkaEndpoint },
+            };
+            // Create the consumer
+            using (var consumer = new Consumer<Null, string>(consumerConfig, null, new StringDeserializer(Encoding.UTF8)))
+            {
+                // Subscribe to the OnMessage event
+                consumer.OnMessage += (obj, msg) =>
+                {
+                    Console.WriteLine($"Received: {msg.Value}");
+                };
+
+                // Subscribe to the Kafka topic
+                consumer.Subscribe(new List<string>() { kafkaTopic });
+
+                // Handle Cancel Keypress 
+                var cancelled = false;
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true; // prevent the process from terminating.
+                    cancelled = true;
+                };
+
+                Console.WriteLine("Ctrl-C to exit.");
+
+                // Poll for messages
+                while (!cancelled)
+                {
+                    consumer.Poll();
+                }
+            }
+
+        }
+    }
+}
---
+++ KafkaExperimentor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+using System.Collections.Generic;
+
+namespace KafkaExperimentor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kafkaEndpoint = "localhost:9092";
+
+            // The Kafka topic we'll be usingB
+            string kafkaTopic = "javainuse-topic";
+
+            // Create the producer configuration
+            var producerConfig = new Dictionary<string, object> { { "bootstrap.servers", kafkaEndpoint } };
+
+            // Create the producer
+            using (var producer = new Producer<Null, string>(producerConfig, null, new StringSerializer(Encoding.UTF8)))
+            {
+                // Send 10 messages to the topic
+                for (int i = 0; i < 10; i++)
+                {
+                    var message = $"Event {i}";
+                    var result = producer.ProduceAsync(kafkaTopic, null, message).GetAwaiter().GetResult();
+                    Console.WriteLine($"Event {i} sent on Partition: {result.Partition} with Offset: {result.Offset}");
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
